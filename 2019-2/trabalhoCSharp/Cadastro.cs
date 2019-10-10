47dbe32b2f4ee3fd2e36397fb57401e08915acdb
GabrielCarlosLopes <gabriel.lopes@rede.ulbra.br>
2019-10-10 11:36:51 -0300

    up

---
+++ 2019-2/trabalhoCSharp/Cadastro.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trabalho
+{
+    class Cadastro
+    {
+  
+        public int Codigo;
+        public String Nome;
+        public String Endereco;
+        public String Telefone;
+        public String Email;
+
+        public static int codIncrement;
+
+        public override string ToString()
+        {
+            return "\n"+Codigo+": "+Nome+", "+Endereco+", "+Telefone+", "+Email;
+        }
+
+        public void ler()
+        {
+            Codigo = codIncrement;
+            codIncrement++;
+            Console.WriteLine("\nSEU CODIGO EH: " + Codigo);
+            Console.WriteLine("\nDIGITE O NOME: ");
+            Nome = Console.ReadLine();
+            Console.WriteLine("\nDIGITE O ENDERECO: ");
+            Endereco = Console.ReadLine();
+            Console.WriteLine("\nDIGITE O TELEFONE: ");
+            Telefone = Console.ReadLine();
+            Console.WriteLine("\nDIGITE O EMAIL: ");
+            Email = Console.ReadLine();  
+        }
+
+        public static void consultar(Cadastro[] lista, String nome)
+        {
+            for(int i=0; i<codIncrement; i++)
+            {
+                if (nome.Equals(lista[i].Nome))
+                {
+                    Console.WriteLine(lista[i].Codigo+", "+
+                        lista[i].Nome+", "+
+                        lista[i].Endereco+", "+
+                        lista[i].Email);
+                    break;
+                }
+            }
+        }
+
+        public static void mostrar(Cadastro[] lista)
+        {
+            for(int i=0; i<codIncrement; i++)
+            {
+                Console.WriteLine(lista[i]);
+            }
+        }
+
+        
+
+        
+    }
+}
---
+++ 2019-2/trabalhoCSharp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Trabalho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu;
+            int quantidade = 0;
+            int tam = 10;
+            int codAlter;
+            String nomeConsult;
+
+   
+            Cadastro[] lista = new Cadastro[tam];
+
+
+            while (true)
+            {
+                Console.WriteLine("\n=====Cadastro de Clientes=====");
+                Console.WriteLine("\nEscolha uma Opcao");
+                Console.WriteLine("[1]INCLUIR");
+                Console.WriteLine("[2]ALTERAR DADOS POR CODIGO");
+                Console.WriteLine("[3]CONSULTA POR NOME");
+                Console.WriteLine("[4]LISTAR");
+                Console.WriteLine("[5]SAIR");
+                menu = Int32.Parse(Console.ReadLine());
+                Console.Clear();
+
+                switch (menu)
+                {
+                    case 1:
+                        Cadastro cadastro = new Cadastro();
+                        cadastro.ler();
+                        lista[quantidade] = cadastro;
+                        quantidade++;
+                        break;
+                    case 2:
+                        Console.WriteLine("\nDIGITE O CODIGO REFERENTE AO QUE DESEJA ALTERAR: ");
+                        codAlter = Int32.Parse(Console.ReadLine());
+
+                        for (int i = 0; i < quantidade; i++)
+                        {
+                            if (codAlter.Equals(lista[i].Codigo))
+                            {
+                                Cadastro cadastro1 = new Cadastro();
+                                cadastro1.ler();
+                                lista[i] = cadastro1;
+                            }
+                        }
+                        break;
+
+                    case 3:
+                        Console.WriteLine("\nDIGITE O NOME QUE DESEJA PROCURAR: ");
+                        nomeConsult = Console.ReadLine();
+
+                        Cadastro.consultar(lista, nomeConsult);
+                        break;
+                    case 4:
+                        Cadastro.mostrar(lista);
+                        break;
+                    case 5:
+                        Console.WriteLine("\nPROGRAMA ENCERRADO");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("\nOPCAO INVALIDA");
+                        break;
+                }
+            };
+        }
+    }
+}
